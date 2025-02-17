713c964fa8b4092e8e187c5fea6f60dfbf358b8a
ccy <ccccy096@gmail.com>
2025-02-17 22:21:34 +0800

    switch to dune proj as ima about to expand 📈

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 24 game cracker.
 
-It's a toy project for me to practice ocaml. Not much codes or tests but a lot of trial and err. 
+It's a toy project for me to learn ocaml. Not much codes or tests but a lot of trial and err. 
--- bin/main.ml
+++ bin/main.ml
@@ -3,17 +3,17 @@ let nums = ref []
 let usage_msg = "crack24 -t <target> -- <n1> <n2> [<nn>]..."
 
 let parse_numbers args =
-  try List.map int_of_string args with
-  | Failure _ -> failwith "Invalid number provided. Please provide integers only."
-;;
+  try List.map int_of_string args
+  with Failure _ ->
+    failwith "Invalid number provided. Please provide integers only."
 
 let speclist =
-  [ "-t", Arg.Set_int target, "Target integer"
-  ; ( "--"
-    , Arg.Rest_all (fun args -> nums := parse_numbers args)
-    , "List of integers to play with" )
+  [
+    ("-t", Arg.Set_int target, "Target integer");
+    ( "--",
+      Arg.Rest_all (fun args -> nums := parse_numbers args),
+      "List of integers to play with" );
   ]
-;;
 
 let anon_fun _ = failwith "Please use the flags! See --help for usage."
 
@@ -23,9 +23,8 @@ let () =
   Printf.printf "Target: %d\nNumbers: " !target;
   List.iter (Printf.printf "%d ") !nums;
   print_newline ();
-  match Solver.it_takes_two !target !nums with
+  match Crack24.Solver.it_takes_two !target !nums with
   | [] -> print_endline "No possible path found."
   | paths ->
-    print_endline "All possible paths:";
-    List.iter (fun path -> Printf.printf "%s%d\n" path !target) paths
-;;
+      print_endline "All possible paths:";
+      List.iter (fun path -> Printf.printf "%s%d\n" path !target) paths
